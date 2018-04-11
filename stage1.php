794dfc1254ca999274ca9a015633fbb76e7f4ea8
Faith Uhie <37807424+Fayoung01@users.noreply.github.com>
2018-04-11 12:51:04 +0100

    Add files via upload

---
+++ stage1.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>Stage 1 Task</title>
+	<link rel="stylesheet" type="text/css" href="stage1.css">
+	<meta content="text/html; charset=utf-8">
+</head>
+
+<body>
+
+	<header>
+		<nav><p align="right">
+			<span>Home |</span>
+			<span>About Us  |</span>
+			<span>Contact </span></p>
+		</nav>
+	</header>
+
+	<hr color="white">
+	<br>
+	<content>	
+		<div>
+			<div class="name"><strong>FOOTBALL.HNG4</strong></div>>
+
+			<br><br><br><br><br>
+		</div>
+		<div>
+			<div class="enter">ENTER</div>
+			<br>
+		</div>
+		<br><br><br><br><br>
+		
+		<div class="date"><?php echo (date("l, M d, Y."))."<br> &emsp;"
+		.(date ("G:i a"));
+		?></div>
+	</content>		
+</body>
+</html>
